21625adc54db1e5392a352c749da8a2abfc34256
AzatBaisynov <azat.baisynov@gmail.com>
2023-12-18 17:22:45 +0600

    todo: update page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Aside from './Aside'
 import Create from './Create'
 import Delete from './Delete'
+import Update from './Update';
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Create />} />
         <Route path="/delete" element={<Delete />} />
+        <Route path="/update/:id" element={<Update />} />
       </Routes>
     </>
   )
---
+++ src/Update.jsx
@@ -0,0 +1,194 @@
+import { useEffect, useRef, useState } from "react";
+import { axiosGet, url } from "./api/axios.request";
+import axios from "axios";
+import { useParams } from 'react-router-dom'
+
+const Update = () => {
+
+	const { id } = useParams()
+
+	const [oldCard, setOldCard] = useState({})
+
+	useEffect(() => {
+		/* TODO: Axios запрос на получение товара со всеми его данными */
+
+		/* TODO: Axios запрос на получение товара со всеми его данными */
+	}, [])
+
+	const [cardInfo, setCardInfo] = useState({
+		name: '',
+		description: '',
+		color_description: '',
+		color: '',
+		product_code: ''
+	})
+
+	const cardId = useRef('')
+
+	const [cardImages, setCardImages] = useState({
+		mainImage: {},
+		mainImages: {},
+		firsBottom: {},
+		secondBottom: {}
+	})
+
+	const sendInfo = async () => {
+		try {
+			const headers = {
+				'Content-Type': 'application/json',
+			}
+
+			const body = {
+				"name": cardInfo.name,
+				"description": cardInfo.description,
+				"color_description": cardInfo.color_description,
+				"color": cardInfo.color,
+				"product_code": cardInfo.product_code
+			}
+
+			const { status, data } = await axios.post(`${url}/product/create`, body, {
+				headers: headers
+			})
+			if (status === 201) {
+				return data.id
+			}
+		} catch (error) {
+			return error.message
+		}
+	}
+
+	const sendMainImage = async () => {
+		try {
+			const headers = {
+				'Content-Type': 'multipart/form-data',
+			}
+
+			const formData = {
+				"file": cardImages.mainImage,
+			}
+			const { status } =
+				await axios.post(`${url}/product/main-image/${cardId.current}`, formData, {
+					headers: headers
+				})
+			return status
+		} catch (error) {
+			alert('Ошибка при загрузке картинки')
+		}
+	}
+
+	const sendMainImages = async () => {
+		try {
+			const headers = {
+				'Content-Type': 'multipart/form-data',
+			}
+
+			let formData = new FormData()
+			for (let i = 0; i < cardImages.mainImages.length; i++) {
+				formData.append('files', cardImages.mainImages[i])
+			}
+
+			const { status } =
+				await axios.post(`${url}/product/main-images/${cardId.current}`, formData, {
+					headers: headers
+				})
+			return status
+		} catch (error) {
+			alert('Ошибка при загрузке картинок')
+		}
+	}
+
+	const createCard = async () => {
+		try {
+			cardId.current = await sendInfo()
+			try {
+				const status1 = await sendMainImage()
+				const status2 = await sendMainImages()
+				if (status1 === 201 && status2 === 201) {
+					alert('Карточка создана и картинки загружены')
+				}
+			} catch {
+				alert('Ошибка при попытке загрузить картинку')
+			}
+		} catch (error) {
+			alert(error)
+		}
+	}
+
+	return (
+		<div className="create">
+			<div className="create__container container">
+				<div className="create__wrapper">
+					<form className="create__form" action="">
+						<p className="create__title">Загрузить фото</p>
+						<input className="create__inputs" type="text"
+							placeholder="Название"
+							value={ oldCard?.name }
+							onInput={e => {
+								/*  TODO: Добавить двустороннюю связь  */
+								setOldCard({ ...oldCard, name: e.target.value })
+								setCardInfo({ ...cardInfo, name: e.target.value })
+							}}
+						/>
+						<textarea className="create__inputs create__inputs--textarea"
+							cols="30" rows="10" placeholder="Описание"
+							onInput={e =>
+								setCardInfo({ ...cardInfo, description: e.target.value })}
+						></textarea>
+						<input className="create__inputs" type="text"
+							placeholder="Название цвета"
+							onInput={e =>
+								setCardInfo({ ...cardInfo, color_description: e.target.value })}
+						/>
+						<input className="create__inputs" type="text"
+							placeholder="Цвет #FFFFFF"
+							onInput={e =>
+								setCardInfo({ ...cardInfo, color: e.target.value })}
+						/>
+						<input className="create__inputs" type="text"
+							placeholder="Код продукта"
+							onInput={e =>
+								setCardInfo({ ...cardInfo, product_code: e.target.value })}
+						/>
+					</form>
+					<div className="create__block">
+						<div className="create__inner">
+							<form className="create__form main-image" action="">
+								<p className="create__title">Загрузить фото</p>
+								<input className="create__inputs" type="file"
+									onInput={e =>
+										setCardImages({
+											...cardImages, mainImage: e.target.files[0]
+										})}
+								/>
+							</form>
+
+							<form className="create__form main-image" action="">
+								<p className="create__title">Загрузить фото для меленьких блоков</p>
+								<input className="create__inputs" type="file" multiple
+									onInput={e =>
+										setCardImages({ ...cardImages, mainImages: e.target.files })}
+								/>
+							</form>
+						</div>
+						<div className="create__inner">
+							<div>
+								<img src={`${url}/file/${oldCard?.mainImage?.id}`} alt="" />
+								<form className="create__form main-image" action="">
+									<p className="create__title">Загрузить фото для 360</p>
+									<input className="create__inputs" type="file" />
+								</form>
+							</div>
+							<form className="create__form main-image" action="">
+								<p className="create__title">Загрузить фото для 360</p>
+								<input className="create__inputs" type="file" />
+							</form>
+						</div>
+					</div>
+				</div>
+				<button className="create__btn" onClick={createCard}>Создать</button>
+			</div>
+		</div>
+	)
+}
+
+export default Update
